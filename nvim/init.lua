762318ce88a1d7a33b0c6aa0fed1b8e4570aae58
adamf <adam.a.french@outlook.com>
2026-03-12 14:28:46 +0000

    revert ecefd423fd1fed5b9ebc6ccb046c1a647ebb6437
    revert Start neovim config from scratch

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,18 +1,2 @@
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.fn.isdirectory(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Set leader key before lazy (important!)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
--- Load lazy with your plugins
-require("lazy").setup("plugins")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
