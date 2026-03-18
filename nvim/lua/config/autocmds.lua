f989f2e6136941dc334690ece3f7e908ea313f79
Adam French <adam.a.french@outlook.com>
2026-03-18 18:35:50 +0000

    Tile auto with multiple files

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- When opening multiple files, tile them vertically (side by side)
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argc() == 2 then
+      vim.cmd("vertical ball")
+    end
+  end,
+})
