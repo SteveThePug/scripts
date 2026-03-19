c0ea9ecf0f1e0cd6033623c80544cec590cba97f
Adam French <adam.a.french@outlook.com>
2026-03-19 00:37:32 +0000

    Change fadelevel

--- nvim/lua/plugins/vimade.lua
+++ nvim/lua/plugins/vimade.lua
@@ -3,7 +3,7 @@ return {
 	event = "WinNew",
 	opts = {
 		recipe = { "minimalist", { animate = true } },
-		fadelevel = 0.6,
+		fadelevel = 0.8,
 		blocklist = {
 			claude_terminal = {
 				buf_opts = { ft = "snacks_terminal" },
